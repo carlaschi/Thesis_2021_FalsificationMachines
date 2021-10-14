5a3650c670edbc8554678b61279cf21a0b9a058d
Carla Schindler <carla.schindler@student.ru.nl>
2021-10-14 14:30:13 +0200

    Initial commit
    Testing project that counts words of URL

---
+++ src/nl/ru/ai/thesis/Controller.java
@@ -0,0 +1,15 @@
+package nl.ru.ai.thesis;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void pressButton(ActionEvent event){
+       System.out.println("Hello World");
+    }
+
+
+    public void countWords(ActionEvent actionEvent) {
+
+    }
+}
